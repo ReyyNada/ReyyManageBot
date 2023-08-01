f91177237152d45d70bccd1c2d9575c8d77ebb2f
dappa <94505216+ReyyNada@users.noreply.github.com>
2023-08-02 02:59:17 +0700

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -1023,7 +1023,7 @@ def main():
                     [
                         InlineKeyboardButton(
                             text="➕ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ➕",
-                            url="https://t.me/groupcontrollertgbot?startgroup=true"
+                            url="https://t.me/mahaxiaobot?startgroup=true"
                             )
                        ]
                 ]
