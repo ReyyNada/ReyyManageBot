8bb661ce1f3e9e3c03e7fe2a877ce9a91b9449d0
dappa <94505216+ReyyNada@users.noreply.github.com>
2023-08-16 19:54:19 +0700

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -58,7 +58,7 @@ async def mentionall(event):
             break
         usrnum += 1
         usrtxt += f"😘[{usr.first_name}](tg://user?id={usr.id}), "
-            if usrnum == 5:
+        if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n{usrtxt}"
                 await client.send_message(chat_id, txt)
