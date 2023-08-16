b909e4beca12a1ff4b83997745973d96b491ddf6
dappa <94505216+ReyyNada@users.noreply.github.com>
2023-08-16 19:52:18 +0700

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -9,10 +9,10 @@ from MukeshRobot import telethn as client
 
 spam_chats = []
 
-@client.on(events.NewMessage(pattern="^@tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@all ?(.*)"))
+
+@client.on(events.NewMessage(pattern="^/all ?(.*)"))
 @client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@mention ?(.*)"))
+@client.on(events.NewMessage(pattern="^/mention ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
@@ -57,9 +57,8 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"🥵[{usr.first_name}](tg://user?id={usr.id}), "
-        usrtxt += f"🥵[{usr.first_name}](tg://user?id={usr.id}), "
-        usrtxt += f"🥵[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"😘[{usr.first_name}](tg://user?id={usr.id}), "
+            if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n{usrtxt}"
                 await client.send_message(chat_id, txt)
